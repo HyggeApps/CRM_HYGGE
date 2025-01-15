9c1ad5cce769e33d1afc6390b0dfeab7633e6b7e
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-01-15 15:30:27 -0300

    Update login.py

--- login.py
+++ login.py
@@ -15,30 +15,5 @@ def init_firebase():
 # Inicializar Firebase
 init_firebase()
 
-# Função para listar dados do Firestore
-def list_firestore_data(collection_name):
-    try:
-        db = firestore.client()
-        docs = db.collection(collection_name).stream()
-        data = [{doc.id: doc.to_dict()} for doc in docs]
-        return data
-    except Exception as e:
-        st.error(f"Erro ao acessar o Firestore: {e}")
-        return []
-
-# Interface do Streamlit
-st.title("Listar Conteúdo do Firestore")
-
-# Entrada para o nome da coleção
-collection_name = st.text_input("Digite o nome da coleção:", "users")
-
-# Botão para listar dados
-if st.button("Listar Dados"):
-    st.write(f"Listando documentos da coleção: {collection_name}")
-    data = list_firestore_data(collection_name)
-    if data:
-        for index, doc in enumerate(data):
-            st.write(f"Documento {index + 1}:")
-            st.json(doc)
-    else:
-        st.warning("Nenhum documento encontrado na coleção ou a coleção não existe.")
+db = firestore.client()
+st.write(db)
