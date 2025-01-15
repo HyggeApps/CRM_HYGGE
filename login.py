7147d81113ec24b85aa4760a21aac8f0b0883afb
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-01-15 12:09:19 -0300

    Update login.py

--- login.py
+++ login.py
@@ -4,29 +4,6 @@ from firebase_admin import credentials, db
 import datetime
 import pytz
 
-utc_time = datetime.datetime.now(pytz.utc)
-st.write("Horário UTC:", utc_time)
-# Obter as credenciais do Streamlit Secrets
-firebase_credentials = st.secrets['firebase2']
+firebase_credentials = st.secrets["firebase2"]
 
 st.write(firebase_credentials)
-
-# Verificar se o Firebase já está inicializado
-if not firebase_admin._apps:
-    # Inicializar o Firebase
-    cred = credentials.Certificate(dict(firebase_credentials))
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://crm-hygge-default-rtdb.firebaseio.com/'
-    })
-
-# Função para acessar os usuários
-def fetch_users():
-    ref = db.reference('users')
-    return ref.get() or {}
-
-# Testar a conexão
-try:
-    users = fetch_users()
-    st.write("Usuários no banco de dados:", users)
-except Exception as e:
-    st.error(f"Erro ao acessar o banco de dados: {e}")
