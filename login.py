e9f75badf55a45888762af496b3e2f31b9c67030
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-01-15 11:41:31 -0300

    --

--- login.py
+++ login.py
@@ -7,14 +7,14 @@ firebase_credentials = st.secrets["firebase"]
 
 
 st.write(firebase_credentials)
-'''# Verificar se o Firebase já está inicializado
+# Verificar se o Firebase já está inicializado
 if not firebase_admin._apps:
     # Inicializar o Firebase
     cred = credentials.Certificate(dict(firebase_credentials))
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://crm-hygge-default-rtdb.firebaseio.com/'
     })
-
+'''
 # Função para acessar os usuários
 def fetch_users():
     ref = db.reference('users')
