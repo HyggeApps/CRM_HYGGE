3593d6cb0036c459d74863cd2ddd09e070c299c9
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-01-15 11:09:37 -0300

    --

---
+++ Libs/json_firebase.py
@@ -0,0 +1,21 @@
+import requests
+
+# URL de download direto do Google Drive
+url = "https://drive.google.com/uc?id=1UIeLYKWoMn1yuUsZrnMRQlYfthyuzlt_&export=download"
+
+# Nome do arquivo local para salvar
+file_name = "firebase_credentials.json"
+
+try:
+    # Fazer o download do arquivo
+    response = requests.get(url)
+    response.raise_for_status()  # Verificar se houve erro HTTP
+
+    # Salvar o arquivo localmente
+    with open(file_name, "wb") as file:
+        file.write(response.content)
+
+    print(f"Arquivo baixado com sucesso: {file_name}")
+
+except requests.exceptions.RequestException as e:
+    print(f"Erro ao baixar o arquivo: {e}")
