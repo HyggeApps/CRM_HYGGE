fc8e7a5fee9a7c3509dbd7ee38b855e5cc0170f3
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-01-15 15:21:23 -0300

    Update login.py

--- login.py
+++ login.py
@@ -2,30 +2,18 @@ import os
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
-import streamlit_authenticator as stauth
-from streamlit_authenticator.utilities.hasher import Hasher
 
 # Defina a variável de ambiente explicitamente
 os.environ["GOOGLE_CLOUD_PROJECT"] = "crm-hygge"
-
 # Inicializar o Firebase
 def init_firebase():
     if not firebase_admin._apps:  # Evitar inicializações múltiplas
         cred = credentials.Certificate("crm-hygge-firebase-adminsdk-idxya-52f8b98280.json")
+        firebase_admin.initialize_app(cred, {
+            'projectId': 'crm-hygge'  # Substitua pelo seu Project ID
+        })
         firebase_admin.initialize_app(cred)
 
+# Inicializar o Firebase
 # Inicializar Firebase
 init_firebase()
-
-# Adicionar usuário ao Firestore
-def add_user_to_firestore(username, name, password):
-    db = firestore.client()
-    # Criar hash da senha corretamente
-    db.collection("users").document(username).set({
-        "username": username,
-        "name": name,
-        "password": password
-    })
-
-# Exemplo de adição de usuário
-add_user_to_firestore("user3", "User Three", "password123")
