f340cf61c6f52f511530bfab498a7a00914b6b28
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-01-16 14:10:31 -0300

    --

--- login.py
+++ login.py
@@ -1,3 +1,6 @@
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
 import streamlit as st
 from modules import (
     cadastro_usuarios,
@@ -10,76 +13,161 @@ from modules import (
     cadastro_subempresas,
     cadastro_leads,
 )
-from utils import authentication as auth
+from utils import functions as funcs
+from streamlit_authenticator.utilities import (CredentialsError,
+                                               ForgotError,
+                                               Hasher,
+                                               LoginError,
+                                               RegisterError,
+                                               ResetError,
+                                               UpdateError)
+import json
 
-# Configuração inicial da página
+# Configuração da página
 st.set_page_config(
-    page_title="Gerenciador de Cadastros",
-    layout="wide"
+    page_title='HYGGE - Ambiente de certificação ambiental', layout='wide',
+    page_icon='https://hygge.eco.br/wp-content/uploads/2022/06/Logo_site.png'
 )
 
-# Autenticação
-name, authentication_status, username = auth.configurar_login()
+image1 = 'https://hygge.eco.br/wp-content/uploads/2024/07/white.png'
+image_width_percent = 40
 
-if authentication_status:
-    st.sidebar.title(f"Bem-vindo, {name}!")
+html_code1 = f"""
+    <div style="display: flex; justify-content: center; align-items: center; height: 100%; ">
+        <img src="{image1}" alt="Image" style="width: {image_width_percent}%;"/>
+    </div>
+"""
+st.sidebar.markdown(html_code1, unsafe_allow_html=True)
 
-    # Título Principal
-    st.title("Gerenciador de Cadastros")
+custom_css = """
+    <style>
+    .main {
+        max-width: 80%;
+        margin: 0 auto;
+    }
+    section[data-testid="stSidebar"] {
+        width: 400px !important;
+    }
+    </style>
+"""
+st.markdown(custom_css, unsafe_allow_html=True)
+st.sidebar.markdown('------')
 
-    # Configurar as abas
-    tabs = st.tabs([
-        "Usuários",
-        "Empresas (Matriz)",
-        "Empresas (Sub-empresas)",
-        "Produtos",
-        "Templates",
-        "Contatos",
-        "Leads",
-        "Oportunidades",
-        "Orçamentos"
-    ])
+# Criar um arquivo temporário com os usuários do MongoDB
+#temp_config_path = funcs.create_temp_config_from_mongo(mongo_uri, db_name, collection_name)
 
-    # Aba: Cadastro de Usuários
-    with tabs[0]:
-        st.header("Cadastro de Usuários")
-        cadastro_usuarios.gerenciamento_usuarios()
+# Carregar e verificar as chaves no arquivo config.yaml
+config_file = "utils\config.yaml"
+config_data = funcs.load_config_and_check_or_insert_cookies("utils\config.yaml")
 
-    # Aba: Cadastro de Empresas
-    with tabs[1]:
-        st.header("Cadastro de Empresas (Matriz)")
-        cadastro_empresas.gerenciamento_empresas()
+with st.sidebar:
+    
+    # Loading config file
+    with open(config_file, 'r', encoding='utf-8') as file:
+        config = yaml.load(file, Loader=SafeLoader)
 
-    with tabs[2]:
-        st.header("Cadastro de Empresas (Sub-empresas)")
-        cadastro_subempresas.gerenciamento_subempresas()
+    # Pre-hashing all plain text passwords once
+    # stauth.Hasher.hash_passwords(config['credentials'])
 
-    # Aba: Cadastro de Produtos
-    with tabs[3]:
-        st.header("Cadastro de Produtos")
-        cadastro_produtos.gerenciamento_produtos()
+    with st.sidebar:
+        # Creating the authenticator object
+        authenticator = stauth.Authenticate(
+            config['credentials'],
+            config['cookie']['name'],
+            config['cookie']['key'],
+            config['cookie']['expiry_days']
+        )
 
-    with tabs[4]:
-        st.header("Cadastro de Templates")
-        cadastro_templates.gerenciamento_templates()
+        # authenticator = stauth.Authenticate(
+        #     '../config.yaml'
+        # )
 
-    with tabs[5]:
-        st.header("Cadastro de Contatos")
-        cadastro_contatos.gerenciamento_contatos()
+        # Creating a login widget
+        try:
+            authenticator.login()
+        except LoginError as e:
+            st.error(e)
+            
+    # Autenticando usuário
+    if st.session_state['authentication_status']:
+        if 'admin' in st.session_state["roles"]:
+            st.info(f'Bem-vindo(a), **{st.session_state["name"]}**!')
+            st.info('Este é o ambiente de **admin** para preenchimento das informações referentes ao EVTA de projetos.')
+        else:
+            st.info(f'Bem-vindo(a), **{st.session_state["name"]}**!')
+            st.info('Este é o ambiente de **usuário** para preenchimento das informações referentes ao EVTA de projetos.')
 
-    with tabs[6]:
-        st.header("Cadastro de Leads")
-        cadastro_leads.gerenciamento_leads()
+    elif st.session_state['authentication_status'] is False:
+        st.error('Usuário e/ou Senha inválido(s).')
+    elif st.session_state['authentication_status'] is None:
+        st.warning('Por favor, entre com o seu usuário e senha.')
 
-    with tabs[7]:
-        st.header("Cadastro de Oportunidades")
-        cadastro_oportunidades.gerenciamento_oportunidades()
+@st.cache_data
+def read_json_creditos(path):
+    with open(path, 'r', encoding='utf-8') as file:
+        creditos = json.load(file)
+        return creditos
 
-    with tabs[8]:
-        st.header("Cadastro de Orçamento")
-        cadastro_orcamentos.gerenciamento_orcamentos()
+@st.cache_data
+def read_json_pontuacao_creditos(path):
+    with open(path, 'r', encoding='utf-8') as file:
+        pontuacao_creditos = json.load(file)
+        return pontuacao_creditos
 
-elif authentication_status is False:
-    st.error("Usuário ou senha incorretos.")
-elif authentication_status is None:
-    st.warning("Por favor, insira suas credenciais.")
+if st.session_state['authentication_status']:
+    if 'admin' in st.session_state["roles"]:
+        
+        # Título Principal
+        st.title("Gerenciador de Cadastros")
+
+        # Configurar as abas
+        tabs = st.tabs([
+            "Usuários",
+            "Empresas (Matriz)",
+            "Empresas (Sub-empresas)",
+            "Produtos",
+            "Templates",
+            "Contatos",
+            "Leads",
+            "Oportunidades",
+            "Orçamentos"
+        ])
+
+        # Aba: Cadastro de Usuários
+        with tabs[0]:
+            st.header("Cadastro de Usuários")
+            cadastro_usuarios.gerenciamento_usuarios()
+
+        # Aba: Cadastro de Empresas
+        with tabs[1]:
+            st.header("Cadastro de Empresas (Matriz)")
+            cadastro_empresas.gerenciamento_empresas()
+
+        with tabs[2]:
+            st.header("Cadastro de Empresas (Sub-empresas)")
+            cadastro_subempresas.gerenciamento_subempresas()
+
+        # Aba: Cadastro de Produtos
+        with tabs[3]:
+            st.header("Cadastro de Produtos")
+            cadastro_produtos.gerenciamento_produtos()
+
+        with tabs[4]:
+            st.header("Cadastro de Templates")
+            cadastro_templates.gerenciamento_templates()
+
+        with tabs[5]:
+            st.header("Cadastro de Contatos")
+            cadastro_contatos.gerenciamento_contatos()
+
+        with tabs[6]:
+            st.header("Cadastro de Leads")
+            cadastro_leads.gerenciamento_leads()
+
+        with tabs[7]:
+            st.header("Cadastro de Oportunidades")
+            cadastro_oportunidades.gerenciamento_oportunidades()
+
+        with tabs[8]:
+            st.header("Cadastro de Orçamento")
+            cadastro_orcamentos.gerenciamento_orcamentos()
---
+++ utils/functions.py
@@ -0,0 +1,80 @@
+from pymongo.mongo_client import MongoClient
+import yaml
+import tempfile
+import warnings
+warnings.filterwarnings("ignore")
+
+def create_temp_config_from_mongo(mongo_uri, db_name, collection_name):
+    # Conectar ao MongoDB
+    client = MongoClient(mongo_uri)
+    db = client[db_name]
+    collection = db[collection_name]
+
+    # Buscar todos os usuários no MongoDB
+    users_data = collection.find()
+
+    # Criar um dicionário para armazenar os dados do config temporário
+    temp_config_data = {
+        'credentials': {
+            'usernames': {}
+        }
+    }
+
+    # Adicionar usuários do MongoDB ao config temporário
+    for user_data in users_data:
+        username = user_data['username']
+
+        # Preparar os dados do usuário no formato do config.yaml
+        user_yaml_data = {
+            'email': user_data.get('email', ''),
+            'failed_login_attempts': 0,
+            'first_name': user_data.get('name', '').split()[0],
+            'last_name': ' '.join(user_data.get('name', '').split()[1:]),
+            'logged_in': False,
+            'password': user_data.get('password', ''),  # Idealmente, a senha deve ser hasheada
+            'roles': [user_data.get('funcao', 'viewer')]
+        }
+
+        # Adicionar o usuário ao config temporário
+        temp_config_data['credentials']['usernames'][username] = user_yaml_data
+        print(f"Usuário {username} adicionado ao config temporário.")
+
+    # Criar um arquivo temporário para salvar o config.yaml (como texto)
+    with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.yaml', encoding='utf-8') as temp_file:
+        yaml.dump(temp_config_data, temp_file, default_flow_style=False)
+        temp_file_path = temp_file.name
+
+    print(f"Arquivo config temporário criado: {temp_file_path}")
+    return temp_file_path
+
+def load_config_and_check_or_insert_cookies(config_file_path):
+    # Carregar o arquivo YAML existente
+    try:
+        with open(config_file_path, 'r') as file:
+            config_data = yaml.safe_load(file)
+            if config_data is None:
+                config_data = {}  # Caso o arquivo esteja vazio
+    except FileNotFoundError:
+        config_data = {}  # Se o arquivo não existir ainda
+
+    # Verificar e garantir que a seção 'cookie' exista
+    if 'cookie' not in config_data:
+        config_data['cookie'] = {
+            'expiry_days': 0,
+            'key': 'some_signature_key',
+            'name': 'some_cookie_name'
+        }
+        print("Seção 'cookie' criada com valores padrão.")
+
+    # Verificar se a chave 'name' está na seção 'cookie'
+    cookie_name = config_data['cookie'].get('name')
+    if not cookie_name:
+        config_data['cookie']['name'] = 'some_cookie_name'
+        print("Chave 'name' na seção 'cookie' criada com valor padrão.")
+
+    # Salvar o arquivo atualizado
+    with open(config_file_path, 'w') as file:
+        yaml.dump(config_data, file, default_flow_style=False)
+
+    return config_data
+
