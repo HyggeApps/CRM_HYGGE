16ccb07b6ac4043295715e453cd8f8994f5efd18
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-01-16 14:45:48 -0300

    Update login.py

--- login.py
+++ login.py
@@ -117,10 +117,12 @@ if st.session_state['authentication_status']:
     if 'admin' in st.session_state["roles"]:
 
         # Título Principal
-        st.title("Gerenciador de Cadastros")
+        st.title("Gerenciador de oportunidades HYGGE")
+        st.write('----')
 
         # Configurar as abas
         tabs = st.tabs([
+            "Home",
             "Usuários",
             "Empresas (Matriz)",
             "Empresas (Sub-empresas)",
@@ -132,41 +134,46 @@ if st.session_state['authentication_status']:
             "Orçamentos"
         ])
 
-        # Aba: Cadastro de Usuários
+        # Aba: Dashboard do usuário
         with tabs[0]:
+            st.header("Dashboard")
+            st.warning("Em desenvolvimento...")
+
+        # Aba: Cadastro de Usuários
+        with tabs[1]:
             st.header("Cadastro de Usuários")
             cadastro_usuarios.gerenciamento_usuarios()
 
         # Aba: Cadastro de Empresas
-        with tabs[1]:
+        with tabs[2]:
             st.header("Cadastro de Empresas (Matriz)")
             cadastro_empresas.gerenciamento_empresas()
 
-        with tabs[2]:
+        with tabs[3]:
             st.header("Cadastro de Empresas (Sub-empresas)")
             cadastro_subempresas.gerenciamento_subempresas()
 
         # Aba: Cadastro de Produtos
-        with tabs[3]:
+        with tabs[4]:
             st.header("Cadastro de Produtos")
             cadastro_produtos.gerenciamento_produtos()
 
-        with tabs[4]:
+        with tabs[5]:
             st.header("Cadastro de Templates")
             cadastro_templates.gerenciamento_templates()
 
-        with tabs[5]:
+        with tabs[6]:
             st.header("Cadastro de Contatos")
             cadastro_contatos.gerenciamento_contatos()
 
-        with tabs[6]:
+        with tabs[7]:
             st.header("Cadastro de Leads")
             cadastro_leads.gerenciamento_leads()
 
-        with tabs[7]:
+        with tabs[8]:
             st.header("Cadastro de Oportunidades")
             cadastro_oportunidades.gerenciamento_oportunidades()
 
-        with tabs[8]:
+        with tabs[9]:
             st.header("Cadastro de Orçamento")
             cadastro_orcamentos.gerenciamento_orcamentos()
