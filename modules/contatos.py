285b3abb81e1315fb2300904ef8e5d2db5b5d6dc
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-01-30 14:20:19 -0300

    Update contatos.py

--- modules/contatos.py
+++ modules/contatos.py
@@ -3,7 +3,8 @@ import pandas as pd
 from utils.database import get_collection
 
 def exibir_contatos_empresa(user, admin, empresa_cnpj):
-    st.write(11111, empresa_cnpj)
+    st.write(1)
+    st.write(empresa_cnpj)
     collection_contatos = get_collection("contatos")
 
     # Buscar **apenas** os contatos vinculados à empresa atualmente selecionada
