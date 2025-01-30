c59aa03c5de163d494b6cf12753c9a441862b81d
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-01-30 11:28:03 -0300

    Update tarefas.py

--- modules/tarefas.py
+++ modules/tarefas.py
@@ -27,7 +27,6 @@ def exibir_atividades_empresa(user, admin, empresa_cnpj):
                     "tipo_atividade": "Tipo",
                     "titulo": "Título",
                     "contato": "Contato",
-                    "observacoes": "Observações",
                     "descricao": "Descrição",
                     "data_execucao_atividade": "Data Execução",
                     "data_retorno_atividade": "Data Retorno",
@@ -35,8 +34,7 @@ def exibir_atividades_empresa(user, admin, empresa_cnpj):
                 }
             )
 
-            df_atividades = df_atividades[["ID", "Tipo", "Título", "Contato", "Observações",
-                                           "Descrição", "Data Execução", "Data Retorno", "Criado em"]]
+            df_atividades = df_atividades[["Data Execução", "Tipo", "Título","Descrição", "Contato"]]
 
             st.dataframe(df_atividades, hide_index=True, use_container_width=True)
         else:
@@ -57,17 +55,13 @@ def exibir_atividades_empresa(user, admin, empresa_cnpj):
 
                     # Definir data de criação como hoje
                     data_criacao = datetime.today().date()
-                    # Sugerir execução 7 dias após a criação
-                    data_execucao_sugerida = data_criacao + timedelta(days=3)
                     # Criar campo de data de execução com sugestão
-                    data_execucao = st.date_input("Data de Execução *", value=None)
-                    # Data de retorno opcional, sugerindo 7 dias após a execução
-                    data_retorno = st.date_input("Data de Retorno *", value=None)
+                    data_execucao = st.date_input("Data de Execução", value=data_criacao)
 
                     submit_atividade = st.form_submit_button("✅ Adicionar Atividade")
 
                     if submit_atividade:
-                        if titulo and tipo and descricao and contatos_vinculados and data_execucao and data_retorno:
+                        if titulo and tipo and descricao and contatos_vinculados:
                             atividade_id = str(datetime.now().timestamp())  # Gerar um ID único baseado no tempo
                             nova_atividade = {
                                 "atividade_id": atividade_id,
@@ -75,10 +69,8 @@ def exibir_atividades_empresa(user, admin, empresa_cnpj):
                                 "titulo": titulo,
                                 "empresa": empresa_cnpj,
                                 "contato": contato,
-                                "observacoes": observacoes,
                                 "descricao": descricao,
                                 "data_execucao_atividade": data_execucao.strftime("%Y-%m-%d"),
-                                "data_retorno_atividade": data_retorno.strftime("%Y-%m-%d") if data_retorno else None,
                                 "data_criacao_atividade": datetime.now().strftime("%Y-%m-%d")
                             }
                             collection_atividades.insert_one(nova_atividade)
@@ -86,51 +78,3 @@ def exibir_atividades_empresa(user, admin, empresa_cnpj):
                             st.rerun()
                         else:
                             st.error("Preencha os campos obrigatórios: Tipo, Título, Contato, Descrição e Datas.")
-
-            # Se houver atividades cadastradas, exibir popover de edição/remoção
-            if atividades:
-                with st.popover('✏️ Editar Atividade'):
-                    atividade_selecionada = st.selectbox(
-                        "Selecione uma atividade para editar/remover",
-                        options=[f"{a['titulo']} - {a['data_execucao_atividade']}" for a in atividades]
-                    )
-
-                    if atividade_selecionada:
-                        titulo_editar = atividade_selecionada.split(" - ")[0]  # Obter título
-                        atividade_dados = collection_atividades.find_one({"titulo": titulo_editar}, {"_id": 0})
-
-                        if atividade_dados:
-                            with st.form("form_editar_atividade"):
-                                st.subheader("✏️ Editar Atividade")
-                                novo_tipo = st.selectbox("Tipo de Atividade", ["Contato inicial", "Whatsapp", "Ligação", "Email", "Linkedin", "Tarefa", "Reunião", "Blacklist"],
-                                                         index=["Contato inicial", "Whatsapp", "Ligação", "Email", "Linkedin", "Tarefa", "Reunião", "Blacklist"].index(atividade_dados["tipo_atividade"]))
-                                novo_titulo = st.text_input("Título", value=atividade_dados["titulo"])
-                                novo_contato = st.selectbox("Contato Vinculado", lista_contatos, index=lista_contatos.index(atividade_dados["contato"]) if atividade_dados["contato"] in lista_contatos else 0)
-                                novas_observacoes = st.text_area("Observações", value=atividade_dados["observacoes"])
-                                nova_descricao = st.text_area("Descrição", value=atividade_dados["descricao"])
-                                nova_data_execucao = st.date_input("Data de Execução",
-                                                                   value=pd.to_datetime(atividade_dados["data_execucao_atividade"]).date())
-                                nova_data_retorno = st.date_input("Data de Retorno",
-                                                                  value=pd.to_datetime(atividade_dados["data_retorno_atividade"]).date() if atividade_dados["data_retorno_atividade"] else None)
-
-                                submit_editar = st.form_submit_button("💾 Salvar Alterações")
-                                if submit_editar:
-                                    collection_atividades.update_one(
-                                        {"atividade_id": atividade_dados["atividade_id"]},
-                                        {"$set": {
-                                            "tipo_atividade": novo_tipo,
-                                            "titulo": novo_titulo,
-                                            "contato": novo_contato,
-                                            "observacoes": novas_observacoes,
-                                            "descricao": nova_descricao,
-                                            "data_execucao_atividade": nova_data_execucao.strftime("%Y-%m-%d"),
-                                            "data_retorno_atividade": nova_data_retorno.strftime("%Y-%m-%d") if nova_data_retorno else None,
-                                        }}
-                                    )
-                                    st.success("Atividade atualizada com sucesso!")
-                                    st.rerun()
-
-                        #if st.button("🗑️ Remover Atividade"):
-                            #collection_atividades.delete_one({"atividade_id": atividade_dados["atividade_id"]})
-                            #st.success("Atividade removida com sucesso!")
-                            #st.rerun()
