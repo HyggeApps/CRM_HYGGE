d6fe4adb7286061f6656842e75a2f31889a9768f
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-02-11 11:39:13 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,21 +1,155 @@
 import streamlit as st
-import msal
 from msal import ConfidentialClientApplication
 import requests
 import smtplib
+
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
+import streamlit as st
+from modules import (
+    usuarios,
+    empresas,
+    tarefas,
+    meus_numeros,
+    contatos,
+    templates,
+    produtos,
+    negocios,
+    cadastro_orcamentos,
+    cadastro_leads
+)
+from utils import functions as funcs
+from streamlit_authenticator.utilities import (CredentialsError,
+                                               ForgotError,
+                                               Hasher,
+                                               LoginError,
+                                               RegisterError,
+                                               ResetError,
+                                               UpdateError)
+import json
+from utils.database import get_collection
+from streamlit_option_menu import option_menu
+
+@st.cache_data
+def read_json_creditos(path):
+    with open(path, 'r', encoding='utf-8') as file:
+        creditos = json.load(file)
+        return creditos
+
+@st.cache_data
+def read_json_pontuacao_creditos(path):
+    with open(path, 'r', encoding='utf-8') as file:
+        pontuacao_creditos = json.load(file)
+        return pontuacao_creditos
+
+
+# Configuração da página
+st.set_page_config(
+    page_title='CRM HYGGE', layout='wide',
+    page_icon='https://hygge.eco.br/wp-content/uploads/2022/06/Logo_site.png'
+)
+
+image1 = 'https://hygge.eco.br/wp-content/uploads/2024/07/white.png'
+image_width_percent = 40
+
+html_code1 = f"""
+    <div style="display: flex; justify-content: center; align-items: center; height: 100%; ">
+        <img src="{image1}" alt="Image" style="width: {image_width_percent}%;"/>
+    </div>
+"""
+st.sidebar.markdown(html_code1, unsafe_allow_html=True)
+
+
+css = '''
+    <style>
+        [data-testid="stExpander"] div:has(>.streamlit-expanderContent) {
+            overflow-y: auto;
+            max-height: 400px;
+        }
+    </style>
+    '''
+st.markdown(css, unsafe_allow_html=True)
+
+st.markdown(
+    """
+    <style>
+    .stForm {
+        width: 95% !important; /* Ajusta a largura do formulário */
+        margin: auto; /* Centraliza o formulário */
+        padding: 10px; /* Adiciona mais espaço interno */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Adicionando CSS personalizado
+st.markdown(
+    """
+    <style>
+    /* Ajustar o tamanho da fonte global */
+    html, body, [class*="css"] {
+        font-size: 0.875rem !important; /* Reduz a fonte em 2pt */
+    }
+
+    /* Opcional: Ajustar fontes específicas */
+    h1 {
+        font-size: 2.75rem !important; /* Tamanho para H1 */
+    }
+    h2 {
+        font-size: 2.5rem !important; /* Tamanho para H2 */
+    }
+    h3 {
+        font-size: 1.25rem !important; /* Tamanho para H3 */
+    }
+    .stButton > button {
+        font-size: 0.875rem !important; /* Botões */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Adicionando CSS personalizado para ajustar o tamanho da fonte
+st.markdown(
+    """
+    <style>
+    /* Ajustar o tamanho da fonte do menu */
+    .nav-link {
+        font-size: 8px !important; /* Tamanho da fonte desejado */
+    }
+    .nav-link i {
+        font-size: 10px !important; /* Ajustar o tamanho do ícone, se necessário */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+css = """
+<style>
+    /* Aplica rolagem ao conteúdo dentro dos expanders */
+    div[data-testid="stExpanderDetails"] {
+        max-height: 500px !important;  /* Altura máxima antes do scroll */
+        overflow-y: auto !important;  /* Força a rolagem vertical */
+    }
+</style>
+"""
+st.markdown(css, unsafe_allow_html=True)
+
+st.sidebar.markdown('------')
+
+# Criar um arquivo temporário com os usuários do MongoDB
+collection_usuarios = get_collection("usuarios")
+temp_config_path = funcs.create_temp_config_from_mongo(collection_usuarios)
+config_data = funcs.load_config_and_check_or_insert_cookies(temp_config_path)
+
 
 # Verifique se a sessão já tem uma chave 'logado'
 if 'logado' not in st.session_state:
     st.session_state['logado'] = False
 
 if not st.session_state['logado']:
-    st.title("Bem-vindo(a) ao ambiente integrado da Hygge!")
-    st.info("A finalidade desta plataforma integrada é estabelecer uma ponte eficaz entre os diversos departamentos da Hygge, promovendo um núcleo unificado para a criação e gerenciamento de propostas, organização de pastas, composição de e-mails, realização de simulações e elaboração de relatórios. \nEfetue o seu acesso por meio da aba lateral e adote o nosso lema: **\"Be Efficient. Be Better. Be Hygge.\"**")
-    st.sidebar.markdown("")
-    st.sidebar.markdown("")
-    st.sidebar.markdown('------')
-
-    
     
     CLIENT_ID = '2416c40e-33c3-4632-9f43-1884815ceeb8'
     CLIENT_SECRET = '4Lz8Q~0aKVTfTfcWu0h1UWG1FmbiZt-uzGKMlbzL'
@@ -27,8 +161,6 @@ if not st.session_state['logado']:
 
     # Adquirir token
     result = app.acquire_token_for_client(scopes=SCOPE)
-
-    
 
     if "access_token" in result:
         # Usar o token de acesso para chamar o Microsoft Graph API
@@ -73,7 +205,7 @@ if not st.session_state['logado']:
             st.session_state['senha_principal'] = senha_principal
             
         except Exception as e:
-            st.sidebar.error("Falha no login.")
+            st.sidebar.error("Falha no login, senha incorreta.")
 
 # This part is executed if the user is logged in - LOGIN DO USUÁRIO
 if st.session_state.get('logado', False):
@@ -82,14 +214,41 @@ if st.session_state.get('logado', False):
     st.sidebar.markdown('------')
     
     if any(user in email_principal for user in ['rodrigo', 'alexandre', 'paula', 'fabricio','admin']):
-        opcao_servico = st.sidebar.radio(
-            label=f'**Olá {nome_user.upper()}, o que você deseja fazer?**',
-            options=('-- Selecione uma das opções --', 'Geração e envio de propostas', 'Criação de pastas e envios de aceite', 'Envio do email de boas-vindas', 'Envio do relatório/entregável HYGGE'),
-            index=0  # This sets 'Propostas' as the default value
+        st.info(f'Bem-vindo(a), **{st.session_state["name"]}**!')
+        st.info('Este é o ambiente de **admin** para consulta, preenchimento, controle e envio das informações referentes as oportunidades da HYGGE.')
+    
+        # 1. as sidebar menu
+        selected = option_menu(
+            f"CRM HYGGE (Admin)",
+            ["Tarefas", "Empresas", "Contatos", "Negócios", "Templates", "Produtos", "Usuários"],
+            icons=["list-task", "building", "person-lines-fill", "currency-dollar", "file-earmark-text", "archive", "person-add"],
+            menu_icon="cast",
+            default_index=0,
+            styles={
+                #"container": {"background-color": "#3C353F"},  # Background color for the entire menu
+                "menu-title": {"font-size": "16px", "font-weight": "bold"},  # Title styling
+                "nav-link": {"font-size": "12px"},  # Style for links
+                "nav-link-selected": {"font-size": "12px"},  # Style for the selected link
+            },
         )
-    elif any(user in email_principal for user in ['matheus', 'comercial']):
-        opcao_servico = st.sidebar.radio(
-            label=f'**Olá {nome_user.upper()}, o que você deseja fazer?**',
-            options=('-- Selecione uma das opções --', 'Geração e envio de propostas', 'Criação de pastas e envios de aceite'),
-            index=0  # This sets 'Propostas' as the default value
+    else:
+            
+        st.info(f'Bem-vindo(a), **{st.session_state["name"]}**!')
+        st.info('Este é o ambiente de **vendedor** para consulta, preenchimento, controle e envio das informações referentes as oportunidades da HYGGE.')
+
+        # 1. as sidebar menu
+        selected = option_menu(
+            f"CRM HYGGE (Admin)",
+            ["Tarefas", "Empresas", "Contatos", "Negócios", "Templates", "Produtos", "Usuários"],
+            icons=["list-task", "building", "person-lines-fill", "currency-dollar", "file-earmark-text", "archive", "person-add"],
+            menu_icon="cast",
+            default_index=0,
+            styles={
+                #"container": {"background-color": "#3C353F"},  # Background color for the entire menu
+                "menu-title": {"font-size": "16px", "font-weight": "bold"},  # Title styling
+                "nav-link": {"font-size": "12px"},  # Style for links
+                "nav-link-selected": {"font-size": "12px"},  # Style for the selected link
+            },
         )
+
+        
