1bdbcec176bf33e03a0f897dfb2b9c748f783bbd
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-02-11 14:55:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -272,6 +272,7 @@ if st.session_state.get('logado', False):
             )
 
     usuario_ativo = f'{st.session_state["name"]} ({st.session_state["email"]})'
+    st.write(usuario_ativo)
     # Título Principal
     st.title("🗒️ *Customer Relationship Management* (CRM) - HYGGE")
     st.write('----')
