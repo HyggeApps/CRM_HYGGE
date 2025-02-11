19f833899a5ecfbe2c23cb44a0883de0e6a84221
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-02-11 11:59:08 -0300

    Update login.py

--- login.py
+++ login.py
@@ -224,7 +224,6 @@ if not st.session_state['logado']:
 if st.session_state.get('logado', False):
     with st.sidebar:
         if 'admin' in st.session_state["roles"]: 
-            with st.sidebar:
             st.info(f'Bem-vindo(a), **{st.session_state["name"]}**!')
             st.info('Este é o ambiente de **admin** para consulta, preenchimento, controle e envio das informações referentes as oportunidades da HYGGE.')
 
