3ee9f797d36d81eba15cc5649ee78ac9de3d6f49
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-02-14 10:09:13 -0300

    Update negocios.py

--- modules/negocios.py
+++ modules/negocios.py
@@ -407,15 +407,15 @@ def gerenciamento_oportunidades(user):
                                         # Criar uma nova atividade informando que a tarefa foi concluída
                                         cliente_doc = collection_clientes.find_one({"razao_social": row["cliente"]})
                                         if cliente_doc is not None:
-                                            cnpj_cliente = cliente_doc["cnpj"]
+                                            nome_cliente = cliente_doc["razao_social"]
                                         else:
-                                            cnpj_cliente = "Não encontrado"
+                                            nome_cliente = "Não encontrado"
                                         nova_atividade = {
                                             "atividade_id": str(datetime.now().timestamp()),  
                                             "tipo_atividade": "Observação",
                                             "status": "Registrado",
                                             "titulo": f"Oportunidade '{nome_opp}' atualizada",
-                                            "empresa": cnpj_cliente,
+                                            "empresa": nome_cliente,
                                             "descricao": f"O vendedor {user} atualizou a oportunidade '{nome_opp}, novo valor: {novo_valor} e nova data de fechamento: {nova_data_fechamento}'.",
                                             "data_execucao_atividade": datetime.today().strftime("%Y-%m-%d"),
                                             "data_criacao_atividade": datetime.today().strftime("%Y-%m-%d")
