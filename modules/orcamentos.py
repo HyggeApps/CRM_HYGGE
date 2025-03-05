56e31f3c7bcb2be176409148d7cfc53c70b629ac
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-03-05 10:57:34 -0300

    Update orcamentos.py

--- modules/orcamentos.py
+++ modules/orcamentos.py
@@ -139,8 +139,9 @@ def elaborar_orcamento(user):
             )
             if contatos:
                 opcoes_contatos = [f"{c.get('email', 'Sem email')}" for c in contatos]
+                nomes_contatos = [f"{c.get('nome', 'Sem nome')}" for c in contatos]
                 selected_contatos = st.multiselect("**Selecione os contatos da empresa que receberão o orçamento:**", opcoes_contatos, key="orcamento_contatos")
-                nome_contato_principal = st.selectbox("**Selecione o contato principal:**", opcoes_contatos, key="orcamento_contato_principal")
+                nome_contato_principal = st.selectbox("**Selecione o contato principal:**", nomes_contatos, key="orcamento_contato_principal")
             else:
                 st.error("Nenhum contato encontrado para essa empresa.")
                 selected_contatos = []
