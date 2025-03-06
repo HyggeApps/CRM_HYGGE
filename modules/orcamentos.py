f4146ccee11c53d8efbdf99055232bdbd1abe5b7
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-03-06 15:06:37 -0300

    Update orcamentos.py

--- modules/orcamentos.py
+++ modules/orcamentos.py
@@ -415,6 +415,7 @@ def gerenciamento_aceites(user, email, senha):
                             path_proposta_envio = gro.get_versao(f"{selected_negocio}_{negocio_selecionado['_id']}")
                             if path_proposta_envio:
                                 novo_nome_arquivo = os.path.basename(path_proposta_envio)
+                                st.write(novo_nome_arquivo)
                             else:
                                 st.error("Erro ao encontrar o arquivo da proposta.")
                                 return
