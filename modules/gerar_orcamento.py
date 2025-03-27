2c6c5a92cb3e841b96277ad7765d7e6798b10b12
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-03-27 15:04:47 -0300

    --

--- modules/gerar_orcamento.py
+++ modules/gerar_orcamento.py
@@ -706,7 +706,7 @@ def generate_proposal_pdf2(empresa, id, negocio, produtos, valor_negocio, descon
         #st.write(p)
         if 'NBR' in p:
             #st.write('entrei')
-            image_reader = Path(__file__).parent / "PDFs2/NBR_Capa.png"
+            image_reader = Path(__file__).parent / "PDFs2/Capa_NBR_Fast.png"
             break
     if image_reader is None:
         image_reader = Path(__file__).parent / "PDFs2/Capa.png"
