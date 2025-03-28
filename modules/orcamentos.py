842ca42fe96f55247e5202c7adf620524fc3fd35
HyggeApps <153736139+HyggeApps@users.noreply.github.com>
2025-03-28 17:21:42 -0300

    Update orcamentos.py

--- modules/orcamentos.py
+++ modules/orcamentos.py
@@ -889,7 +889,7 @@ def elaborar_orcamento(user, email, senha, admin):
                 if 'NBR Eco' in tipo_empreendimento: nomes_produtos = ['Laudo NBR Economy']
                 elif 'Aditivo' in tipo_empreendimento: nomes_produtos = ['Aditivo de NBR 15.575']
                 elif 'NBR Fast' in tipo_empreendimento: nomes_produtos = ['Laudo NBR Fast']
-                elif 'NBR' in tipo_empreendimento: nomes_produtos = ['Laudo diagnóstico normativo da NBR 15.575']
+                elif 'NBR' in tipo_empreendimento: nomes_produtos = ['NBR Fast - Laudo diagnóstico normativo da NBR 15.575']
                 elif 'Consultoria' in tipo_empreendimento: nomes_produtos = ['Consultoria Hygge']
                 elif 'Auditoria' in tipo_empreendimento and 'Certificação' in tipo_empreendimento: nomes_produtos = ['Certificação EDGE', 'Auditoria EDGE']
                 elif 'Certificação' in tipo_empreendimento and 'EDGE' in tipo_empreendimento: nomes_produtos = ['Certificação EDGE']
